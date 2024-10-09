3aad6810e87aad1bd72b48ae1e7d4ff08fd8eab4
Aysegul Meric <Aysegul.Meric@suratkargo.com.tr>
2024-10-09 08:48:19 +0300

    first init

---
+++ src/Core/OnionArcExample.Application/Interfaces/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using OnionArcExample.Application.Interfaces.Repositories;
+using System;
+using System.Threading.Tasks;
+
+namespace OnionArcExample.Application.Interfaces.UnitOfWork
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        //IDbContextTransaction : EntityFrameworkCore kütüphanesine ihtiyaç vardır.
+        Task<IDbContextTransaction> BeginTransactionAsync();
+        public IProductRepository ProductRepository { get; }
+    }
+}
---
+++ src/Infrastructure/OnionArcExample.Infrastructure/EmailService.cs
@@ -0,0 +1,14 @@
+﻿using OnionArcExample.Application.Interfaces.Services;
+using System;
+
+namespace OnionArcExample.Infrastructure
+{
+    public class EmailService : IEmailService
+    {
+        public bool Send(string to, string message)
+        {
+            Console.WriteLine("mail sent");
+            return true;
+        }
+    }
+}
